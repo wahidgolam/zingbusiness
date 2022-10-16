c5eaae40950bcafda6886fc88a9f0be43e489311
saswatamm <saswatamajumdar3@gmail.com>
2022-10-16 17:14:53 +0530

    V1.1

---
+++ app/src/main/java/com/zingit/restaurant/EmailLogin.java
@@ -0,0 +1,14 @@
+package com.zingit.restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmailLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_login);
+    }
+}
---
+++ app/src/main/java/com/zingit/restaurant/Homescreen_latest.java
@@ -0,0 +1,14 @@
+package com.zingit.restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Homescreen_latest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen_latest);
+    }
+}
